25cc470e2be106ee91239a7820b1b5ea0e2d2cbf
Spatialflunky1 <mjandrus06@gmail.com>
2022-04-11 13:53:52 -0400

    added boundry

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib> // random
 #include <thread> // key input for quitting
 #include <string>
-// Variables used in other threads
+// variables used in other threads
 int key = 0;
 bool running = true;
 
@@ -13,6 +13,7 @@ class Object {
         int bounciness;
         char character;
         float location[4];
+        int angle; // I have no clue how I can use this but I know for a fact it is necessary
         void printInfo() {
             std::cout << "Object info:\n----------------\n";
             std::cout << "Weight: " << weight << "\n";
@@ -22,28 +23,30 @@ class Object {
         }
 };
 
-Object createBall(int weight,char character,int bounciness) {
+Object createBall(int weight,char character,int bounciness,int angle) {
     Object ball;
     ball.weight = weight;
     ball.character = character;
     // 10 should be max bounciness although no stopping making it higher rn
+    // ^ above is a plan for later
     ball.bounciness = bounciness;
     // (currentX, currentY, previousX, previousY)
     ball.location[0]=0;
     ball.location[1]=0;
     ball.location[2]=0;
     ball.location[3]=0;
+    ball.angle = angle;
     return ball;
 }
 
-Object moveObject(Object object,float amountX,float amountY) {
+Object moveObject(Object object,float amountX,float amountY,int rows,int cols) {
     object.location[2] = object.location[0];
     object.location[3] = object.location[1];
-    object.location[0] += amountX;
-    object.location[1] += amountY;
-    //mvaddch(object.location[2],object.location[3],' ');
-    //mvaddch(object.location[0],object.location[1],object.character);
-    mvprintw(object.location[0], object.location[1],std::to_string(object.location[0]).c_str());
+    if (object.location[0] <= rows) object.location[0] += amountX;
+    if (object.location[1] <= cols) object.location[1] += amountY;
+    mvaddch(object.location[2],object.location[3],' ');
+    mvaddch(object.location[0],object.location[1],object.character);
+    //mvprintw(object.location[0], object.location[1],std::to_string(object.location[0]).c_str());
     return object;
 }
 
@@ -56,21 +59,22 @@ void update_key() {
 }
 
 int main() {
-    Object ball = createBall(10,'o',10); 
-    initscr(); // Start curses mode
+    Object ball = createBall(10,'o',10,0); 
+    initscr(); // start curses mode
     noecho();
     curs_set(0);
     int rows,cols;
     getmaxyx(stdscr,rows,cols);
+    // max screen size is outside screen
     rows--;
     cols--;
     std::thread key_thread(update_key);
     while (running) {
-        ball = moveObject(ball,0.0005,0.0005);
+        ball = moveObject(ball,0.0004,0.0004,rows,cols);
         refresh();
     }
     key_thread.join();
-    endwin(); // Exit curses mode
+    endwin(); // exit curses mode
     printf("\x1b[2J"); // clear screen
     printf("\x1b[d"); // return to home position
     return 0;
